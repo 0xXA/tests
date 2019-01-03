cc31fca6383f5a89f5a7b8db7e97327c5ecaad37
Yuvraj Saxena <x27@yuvrajx.me>
2019-01-03 20:35:11 +0530

    tulip: fix 3
    Signed-off-by: Yuvraj Saxena <x27@yuvrajx.me>

--- .func.sh
+++ .func.sh
@@ -2,20 +2,20 @@
 
 [[ "$2" -eq "getboot" ]] && getboot=1
 
-[[ getboot ]] && {
-	wget $3 -O $1.zip >/dev/null 2>&1
-        unzip $2.zip >/dev/null 2>&1
-	if ! command -v unpackbootimg >/dev/null 2>&1; then
-		boottools
-	fi
-	unpackbootimg -i boot.img
-	tar -cJf $1.txz boot.img-zImage
-}
-
-boottools() {
+mkboot() {
 	git clone -q https://github.com/osm0sis/mkbootimg.git
 	cd mkbootimg && {
 		sudo make install
 	        cd -
 	}
+}
+
+[[ getboot ]] && {
+	wget $3 -O $1.zip >/dev/null 2>&1
+        unzip $2.zip >/dev/null 2>&1
+	if ! command -v unpackbootimg >/dev/null 2>&1; then
+		mkboot
+	fi
+	unpackbootimg -i boot.img
+	tar -cJf $1.txz boot.img-zImage
 }
