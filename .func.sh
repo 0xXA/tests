d58b739f7c7c5187ec78c6e30477b30110edd38a
Yuvraj Saxena <ysaxenax@gmail.com>
2020-12-16 16:05:37 +0530

    Update .func.sh

--- .func.sh
+++ .func.sh
@@ -37,10 +37,19 @@ ibinwalk() {
 '
 #[[ cmprf ]] && {
 	#tar -cJf $2.txz $2
-wget https://bigota.d.miui.com/V12.0.5.0.QJOINXM/merlin_in_global_images_V12.0.5.0.QJOINXM_20201115.0000.00_10.0_in_7035a72196.tgz -O merlin.tgz
-split -b 1G merlin.tgz merlin.tgz.part
-for i in merlin.tgz.parta{a..z};do
-	[[ -e `pwd`/$i ]] && rclone copy $i gdrive: ;
-done
+sudo apt install npm
+wget https://dl.google.com/android/repository/android-ndk-r22-linux-x86_64.zip
+unzip android-ndk-r22-linux-x86_64.zip
+export ANDROID_NDK_ROOT=`pwd`/android-ndk-r22/
+git clone --recurse-submodules https://github.com/frida/frida.git
+cd frida
+ls-al *
+make build/frida-android-arm/lib/pkgconfig/frida-core-1.0.pc
+ls -al *
+#wget https://bigota.d.miui.com/V12.0.5.0.QJOINXM/merlin_in_global_images_V12.0.5.0.QJOINXM_20201115.0000.00_10.0_in_7035a72196.tgz -O merlin.tgz
+#split -b 1G merlin.tgz merlin.tgz.part
+#for i in merlin.tgz.parta{a..z};do
+#	[[ -e `pwd`/$i ]] && rclone copy $i gdrive: ;
+#done
 #telegram-upload merlin.tgz.part*
 #}
