fbbb35c3a547f9a88c1ca23fdc19a52669695bf7
Yuvraj Saxena <ysaxenax@gmail.com>
2020-12-16 16:13:14 +0530

    Update .func.sh

--- .func.sh
+++ .func.sh
@@ -39,8 +39,8 @@ ibinwalk() {
 	#tar -cJf $2.txz $2
 sudo apt install npm
 wget https://dl.google.com/android/repository/android-ndk-r21d-linux-x86_64.zip
-unzip android-ndk-r22-linux-x86_64.zip
-export ANDROID_NDK_ROOT=`pwd`/android-ndk-r22/
+unzip -qq android-ndk-r21d-linux-x86_64.zip
+export ANDROID_NDK_ROOT=`pwd`/android-ndk-r21d/
 git clone --recurse-submodules https://github.com/frida/frida.git
 cd frida
 ls
